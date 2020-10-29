7cd018c8e48e8c17c08aaf79573d0affc7ffbb61
E-Shangeraev <e.shangeraev@gmail.com>
2020-10-29 22:46:19 +0700

    Исправил ошибки в коде

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -2,44 +2,17 @@ import React, {Component} from 'react';
 import gotService from '../../services/gotService';
 import Spinner from '../spinner';
 import './itemList.css';
-import gotService from '../../services/gotService';
 import ErrorMessage from '../errorMessage';
-import Spinner from '../spinner/';
 
 
 export default class ItemList extends Component {
-    gotService = new gotService();
-    state = {
-        charList: null
-    }
-
-    componentDidMount() {
-        this.gotService.getAllChars()
-            .then(charList => {
-                this.setState({charList});
-            })
-    }
-
-    renderItems(arr) {
-        // const id = Math.floor(Math.random() * 100);
-        return arr.map((item, i) => {
-            return (
-                <li 
-                    key={i}
-                    className="list-group-item"
-                    onClick={() => this.props.onCharSelected(41 + i)}>
-                    {item.name}
-                </li>
-            )
-        })
-    }
-
     gotService = new gotService();
 
     state = {
         charList: null,
         error: false
     }
+
     componentDidMount() {
         this.gotService.getAllChars()
             .then((charList) => {
@@ -50,18 +23,21 @@ export default class ItemList extends Component {
             })
             .catch(() => {this.onError()});
     }
+
     componentDidCatch(){
         this.setState({
             charList: null,
             error: true
         })
     }
+
     onError(status){
         this.setState({
             charList: null,
             error: true
         })
     }
+
     renderItems(arr) {
         return arr.map(item => {
             const {id, name} = item;
@@ -77,9 +53,7 @@ export default class ItemList extends Component {
         })
     }
 
-
     render() {
-<<<<<<< HEAD
         const {charList, error} = this.state;
 
         if(error){
@@ -92,16 +66,6 @@ export default class ItemList extends Component {
 
         const items = this.renderItems(charList);
 
-=======
-        const {charList} = this.state;
-
-        if(!charList) {
-            return <Spinner />
-        }
-
-        const items = this.renderItems(charList);
- 
->>>>>>> 802bb0c2006dba692bebd3a7d9b02f2caf0b25d6
         return (
             <ul className="item-list list-group">
                 {items}
